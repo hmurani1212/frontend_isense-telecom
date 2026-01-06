04eaca2af9b580b842eb7b59289caba8d934f728
hmurani1212 <120725023+hmurani1212@users.noreply.github.com>
2026-01-07 01:38:10 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,7 +43,7 @@ const Contact = () => {
     setStatus({ type: '', message: '' });
 
     try {
-      const response = await fetch('https://backendisense-telecom-production-d325.up.railway.app/api/send_email', {
+      const response = await fetch('https://backend-isense-telecom.vercel.app/api/send_email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
